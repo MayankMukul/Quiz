c4241cdf3801b6d61b8154e819170766c216a6dd
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-06 01:01:30 +0530

    update

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const question = [{
-        'ques' : 'HTML stands for __________',
-        'a' : 'HyperText Markup Language',
-        'b': 'HyperText Machine Language',
-        'c' : 'HyperText Marking Language',
-        'd' : 'HighText Marking Language',
-        'correct' : 'a'  
-    },{
-        "ques" :"Which element is used for or styling HTML5 layout?",
-        'a' : 'CSS',
-        'b': ' jQuery',
-        'c' : 'JavaScript',
-        'd' : 'PHP',
-        'correct' : 'a'
-    },{
-        "ques": "Javascript is an _______ language?",
-        'a' : 'Object-Oriented',
-        'b': 'Object-Based',
-        'c' : 'Procedural',
-        'd' : 'None of the above',
-        'correct' : 'a'
-    }
+    'ques' : 'HTML stands for __________',
+    'a' : 'HyperText Markup Language',
+    'b': 'HyperText Machine Language',
+    'c' : 'HyperText Marking Language',
+    'd' : 'HighText Marking Language',
+    'correct' : 'a'  
+},{
+    "ques" :"What does CSS stand for?",
+    'a' : 'Computing Style Sheet',
+    'b': ' Creative Style System',
+    'c' : 'Cascading Style Sheet',
+    'd' : 'Creative Styling Sheet',
+    'correct' : 'c'
+},{
+    "ques" :"Which element is used for or styling HTML5 layout?",
+    'a' : 'CSS',
+    'b': ' jQuery',
+    'c' : 'JavaScript',
+    'd' : 'PHP',
+    'correct' : 'a'
+},{
+    "ques" :"Inside which HTML element do we put the JavaScript?",
+    'a' : '<script>',
+    'b': ' <js>',
+    'c' : '<scripting>',
+    'd' : '<javascript>',
+    'correct' : 'a'
+},{
+    "ques": "Javascript is an _______ language?",
+    'a' : 'Object-Oriented',
+    'b': 'Object-Based',
+    'c' : 'Procedural',
+    'd' : 'None of the above',
+    'correct' : 'a'
+}
 ]
 
 let index = 0;
@@ -29,65 +43,63 @@ const ques = document.getElementById("ques");
 const options = document.querySelectorAll('.options');
 
 function loadQues(){
-    if (index==total){
-        return endquiz();
-    }
-    reset();
-    const current_q = question[index];
-    
-    ques.innerHTML = `<div class="question">${index+1}.) ${current_q.ques}</div>`;
-    options[0].nextElementSibling.innerText=current_q.a;
-    options[1].nextElementSibling.innerText=current_q.b;
-    options[2].nextElementSibling.innerText=current_q.c;
-    options[3].nextElementSibling.innerText=current_q.d;
-    return;
+if (index==total){
+    return endquiz();
+}
+reset();
+const current_q = question[index];
+
+ques.innerHTML = `<div class="question">${index+1}.) ${current_q.ques}</div>`;
+options[0].nextElementSibling.innerText=current_q.a;
+options[1].nextElementSibling.innerText=current_q.b;
+options[2].nextElementSibling.innerText=current_q.c;
+options[3].nextElementSibling.innerText=current_q.d;
+return;
 }
 
 function submit() {
-    let answer = getAnswer();
-    let data = question[index];
-    if (answer === data.correct){
-        correct++;
-    } else {
-        wrong++;
-    }
-    index++;
-    loadQues();
-    return;
+let answer = getAnswer();
+let data = question[index];
+if (answer === data.correct){
+    correct++;
+} else {
+    wrong++;
+}
+index++;
+loadQues();
+return;
 
 }
 
 function getAnswer() {
-    let answer ;
-    options.forEach(
-        (input) => {
-            if (input.checked) {
-                answer = input.value;
-            }
+let answer ;
+options.forEach(
+    (input) => {
+        if (input.checked) {
+            answer = input.value;
         }
-    )
-    return answer;
+    }
+)
+return answer;
 }
 
 function reset() {
-    options.forEach(
-        (input) => {
-            input.checked = false; 
-            
-        }
-    )
-    return
+options.forEach(
+    (input) => {
+        input.checked = false;  
+    }
+)
+return
 }
 
 function endquiz() {
-    
-    document.getElementById("box").innerHTML = `
-    <div class="title">
-        <h2>Thank you for playing the quiz</h2>
-        <h3>You Scored : ${correct}/${total} </h3>
-        <button onclick="location.reload()">Play Again !!!</button>
-    </div>`
-    return ;
+document.getElementById("box").innerHTML = `
+<div class="title">
+    <h2>Thank you for playing the quiz</h2>
+    <h3>You Scored : ${correct}/${total} </h3>
+    <button onclick="location.reload()">Play Again !!!</button>
+</div>`
+return ;
 }
 
 loadQues();
